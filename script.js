bb540559f797301517cfedf3d62afdbd87ba3ea6
Kikundi-chetu <ibrahimmbembe21@gmail.com>
2025-05-14 23:54:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById("signupForm").addEventListener("submit", function(e) {
   e.preventDefault(); // zuia kurasa kujireload
 
-  const jina = document.getElementById("jina").value;
-  const email = document.getElementById("email").value;
+  const jina1 = document.getElementById("jinaKwanza").value;
+  const jina2 = document.getElementById("jinaPili").value;
+  const jina3 = document.getElementById("jinaMwisho").value;
+  const simu = document.getElementById("simu").value;
 
   document.getElementById("maelezo").innerHTML =
-    `<p>Asante ${jina}, umejiunga kwa email: ${email}</p>`;
-
-  // Hapa unaweza kuongeza sehemu ya kutuma taarifa kwa database kama Firebase au PHP
+    `<p>Asante ${jina1} ${jina2} ${jina3}, umejiunga kwa namba ya simu: ${simu}</p>`;
 });
